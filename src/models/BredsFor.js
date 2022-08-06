1943a496a8df4d608653282589fb9e8f5c65bb4c
Esneider Ospina <88557374+sneuder@users.noreply.github.com>
2022-08-06 13:48:18 -0500

    Update spelling

--- src/models/BredsFor.js
+++ src/models/BredsFor.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const BredsFor = sequelize.define('bredfor', {
+  const BredsFor = sequelize.define('bredsfor', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
