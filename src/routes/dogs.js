27626bf94ea4159ef26e8bad69ea753792c27f39
Esneider Ospina <88557374+sneuder@users.noreply.github.com>
2022-08-06 00:26:08 -0500

    Add dog routes

---
+++ src/routes/dogs.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const router = Router();
+
+const dogsControllers = require('../controllers/dogsControllers');
+
+router.get('/dogs/allDogs', dogsControllers.getAllDogs);
+router.get('/dogs/:dogID', dogsControllers.getOneDog);
+router.post('/dogs/newDog', dogsControllers.postOneDog);
+router.patch('/dogs/:dogId', dogsControllers.patchOneDog);
+router.delete('/dogs/:dogId', dogsControllers.deleteOneDog);
+
+module.exports = router;
