2aa2df528c3eacac4da34afe11607d684185bf82
Esneider Ospina <88557374+sneuder@users.noreply.github.com>
2022-08-10 12:14:31 -0500

    Improve properties function

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -7,13 +7,36 @@ const postDog = (dog) => {
   }
 };
 
-const postTemps = (dogs) => {
-  let temps = [];
+const postDogs = (dogs) => {
+  return dogs.map(dog => {
+    return {
+      id: dog.id,
+      name: dog.name,
+      life_span: dog.life_span,
+      image: dog.image.url
+    }
+  });
+};
+
+const postAttributes = (dogs, attribute) => {
+  let attrs = [];
+
+  // filterting to have no repeated information
   dogs.forEach(dog => {
-    
+    if (dog[attribute] && dog[attribute] !== ''){
+      const dogAttrs = dog[attribute].replace(/,/g, "").split(" ");
+      dogAttrs.forEach(dogAttr => !attrs.includes(dogAttr) && attrs.unshift(dogAttr));
+    }
+  });
+
+  // formatting data
+  return attrs.map(atrr => {
+    return {[attribute]: atrr}
   });
 }
 
 module.exports = {
-  postDog
+  postDog,
+  postDogs,
+  postAttributes
 }
