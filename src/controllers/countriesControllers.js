2c02961b7cde4753184ef67321fe06b6dd5799d5
Esneider Ospina <88557374+sneuder@users.noreply.github.com>
2022-09-20 22:55:04 -0500

    Add controllers for routes of countries

---
+++ src/controllers/countriesControllers.js
@@ -0,0 +1,27 @@
+const getAllCountries = (req, res) => {
+
+};
+
+const getOneCountry = (req, res) => {
+
+};
+
+const postOneCountry = (req, res) => {
+
+};
+
+const patchOneCountry = (req, res) => {
+
+};
+
+const deleteOneCountry = (req, res) => {
+
+};
+
+module.exports = {
+  getAllCountries,
+  getOneCountry,
+  postOneCountry,
+  patchOneCountry,
+  deleteOneCountry
+};
