692ef20c085d5b463f0cf12e66c166a068531b74
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 02:45:34 -0500

    Fix association functions

--- src/database/utils/setAssociations.js
+++ src/database/utils/setAssociations.js
@@ -1,26 +1,48 @@
 const attrModels = require('../../constants/models');
 
-const temps = (postedDog, temps) => {
-  temps.forEach(async temp => {
-    const [newTemp, oldTemp] = await attrModels.temperament.findOrCreate({
-      where: { temperament:  temp},
+const temps = async (postedDog, dogAttrs) => {
+  dogAttrs.forEach(async dogAttr => {
+    const [newRecord, oldRecord] = await attrModels.temperament.findOrCreate({
+      where: { temperament:  dogAttr}
     });
-  
-    await postedDog.addTemp(newTemp);
+
+    await postedDog.addTemps(newRecord);
   });
 };
 
-const breeds = (postedDog, temps) => {
-  temps.forEach(async temp => {
-    const [newTemp, oldTemp] = await attrModels.temperament.findOrCreate({
-      where: { temperament:  temp},
+const breeds = async (postedDog, dogAttrs) => {
+  dogAttrs.forEach(async dogAttr => {
+    const [newRecord, oldRecord] = await attrModels.breed_group.findOrCreate({
+      where: { breed_group:  dogAttr}
     });
-  
-    await postedDog.addBreed(newTemp);
+
+    await postedDog.addBreeds(newRecord);
+  });
+};
+
+const bredsfor = async (postedDog, dogAttrs) => {
+  dogAttrs.forEach(async dogAttr => {
+    const [newRecord, oldRecord] = await attrModels.bred_for.findOrCreate({
+      where: { bred_for:  dogAttr}
+    });
+
+    await postedDog.addBredsfors(newRecord);
+  });
+};
+
+const countries = async (postedDog, dogAttrs) => {
+  dogAttrs.forEach(async dogAttr => {
+    const [newRecord, oldRecord] = await attrModels.origin.findOrCreate({
+      where: { origin:  dogAttr}
+    });
+
+    await postedDog.addCountries(newRecord);
   });
 };
 
 module.exports = {
   temps,
-  breeds
+  breeds,
+  bredsfor,
+  countries
 };
