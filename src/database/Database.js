3c1bffd19f2deacbe7fb5c39f7a83646f2b48786
Esneider Ospina <sneuder8@gmail.com>
2022-10-17 17:26:22 -0500

    Improvements for Database

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,27 +1,40 @@
+const message = require("../utils/messages");
 const sequelize = require("../db");
 
 const Dogs = sequelize.models.dogs;
-const setAssociations = require('./utils/setAssociations');
-const attrModels = require('../constants/models');
+const setAssociations = require("./utils/setAssociations");
+const attrModels = require("../constants/models");
 
 const postDatabase = async (dog) => {
-  const postedDog = await Dogs.create(dog.formatedDog);
+  try {
+    const postedDog = await Dogs.create(dog.formatedDog);
 
-  setAssociations.temps(postedDog, dog.attributes.temperament);
-  setAssociations.breeds(postedDog, dog.attributes.breed_group);
-  setAssociations.bredsfor(postedDog, dog.attributes.bred_for);
-  setAssociations.countries(postedDog, dog.attributes.origin);
+    setAssociations.temps(postedDog, dog.attributes.temperament);
+    setAssociations.breeds(postedDog, dog.attributes.breed_group);
+    setAssociations.bredsfor(postedDog, dog.attributes.bred_for);
+    setAssociations.countries(postedDog, dog.attributes.origin);
 
-  return postedDog;
+    return postedDog;
+  } catch (e) {
+    return message.error;
+  }
 };
 
 // destroy all data in each table
 const deleteDatabase = async () => {
-  await Dogs.destroy({ truncate: true, cascade: true });
+  try {
+    await Dogs.destroy({ truncate: true, cascade: true });
+  } catch (e) {
+    throw new Error(e);
+  }
 
   const attrKeys = Object.keys(attrModels);
   attrKeys.forEach(async (attrKey) => {
-    await attrModels[attrKey].destroy({ truncate: true, cascade: true });
+    try {
+      await attrModels[attrKey].destroy({ truncate: true, cascade: true });
+    } catch (e) {
+      throw new Error(e);
+    }
   });
 };
 
