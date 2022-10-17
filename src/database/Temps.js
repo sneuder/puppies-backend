220325625357da600ffed2953668a8212b523c94
Esneider Ospina <sneuder8@gmail.com>
2022-10-17 17:19:46 -0500

    Add msg for temps controllers

--- src/database/Temps.js
+++ src/database/Temps.js
@@ -1,44 +1,58 @@
-const sequelize = require('../db');
+const message = require("../utils/messages");
+const sequelize = require("../db");
 const Temps = sequelize.models.temps;
 
 const getAllTemps = async () => {
-  try {return await Temps.findAll()}
-  catch(e) {return {status: 'Temps not found'}}
+  try {
+    return (await Temps.findAll()) || message.noRecords("Temps");
+  } catch (e) {
+    return message.error;
+  }
 };
 
 const getOneTemp = async (tempId) => {
-  try {return await Temps.findByPk(tempId) || {status: `Temp ${tempId} was not found`}} 
-  catch(e) {return {status: `Temp ${tempId} was not found`}}
+  try {
+    return (await Temps.findByPk(tempId)) || message.noRecord("Temp", tempId);
+  } catch (e) {
+    return message.error;
+  }
 };
 
 const postOneTemp = async (temp) => {
-  try{return await Temps.create(temp)}
-  catch(e){return {status: `Temp could not be created`}}
-}
+  try {
+    return await Temps.create(temp);
+  } catch (e) {
+    return message.error;
+  }
+};
 
 const patchOneTemp = async (tempId, tempPortion) => {
-  try{
+  try {
     await Temps.update(tempPortion, {
-      where: {id: tempId}
+      where: { id: tempId },
     });
-    return ({status: `Temp ${tempId} updated`});
-  } catch (e) {return ({status: `Temp ${tempId} not updated`})}
-}
+    return message.updatedRecord("Temp", tempId);
+  } catch (e) {
+    return message.error;
+  }
+};
 
 const deleteOneTemp = async (tempId) => {
   try {
     await Temps.destroy({
-      where: {id: tempId}
+      where: { id: tempId },
     });
-    return ({status: `Temp ${tempId} deleted`});
 
-  } catch (e) {return ({status: `Temp ${tempId} could not be deleted`})}
-}
+    return message.deletedRecord("Temp", tempId);
+  } catch (e) {
+    return message.deletedNoRecord("Temp", tempId);
+  }
+};
 
 module.exports = {
   getAllTemps,
   getOneTemp,
   postOneTemp,
   patchOneTemp,
-  deleteOneTemp
+  deleteOneTemp,
 };
