f5b7d57924eeec6bfa5423cad4fa8ac562d126ae
Esneider Ospina <sneuder8@gmail.com>
2022-10-17 16:51:31 -0500

    Update controller with new msgs

--- src/database/BredsFor.js
+++ src/database/BredsFor.js
@@ -1,33 +1,51 @@
-const sequelize = require('../db');
+const message = require("../utils/messages");
+const sequelize = require("../db");
+
 const BredsFor = sequelize.models.bredsfors;
 
 const getAllBredsFor = async () => {
-  try {return await BredsFor.findAll() || {status: 'There are not BredsFor'};
-  } catch(e) {return {status: 'Something went wrong'}};
+  try {
+    return (await BredsFor.findAll()) || message.noRecords("BredsFor");
+  } catch (e) {
+    return message.error;
+  }
 };
 
 const getOneBredFor = async (bredForId) => {
-  try {return await BredsFor.findByPk(bredForId) || {status: 'There is not BredFor'}
-  } catch(e) {return {status: 'Something went wrong'}}
+  try {
+    return (
+      (await BredsFor.findByPk(bredForId)) ||
+      message.noRecords("BredFor", bredForId)
+    );
+  } catch (e) {
+    return message.error;
+  }
 };
 
 const postOneBredFor = async (newBredFor) => {
-  try {return await BredsFor.create(newBredFor)
-  } catch (e) {return {status: 'BredFor could not be created'}}
+  try {
+    return await BredsFor.create(newBredFor);
+  } catch (e) {
+    return message.error;
+  }
 };
 
 const patchOneBredFor = async (bredForPortion, bredForId) => {
   try {
-    await BredsFor.update(bredForPortion, {where: {id: bredForId}});
-    return {status: `BredFor ${bredForId} updated`}
-  } catch (e) {return {status: `BredFor ${bredForId} not updated`}}
+    await BredsFor.update(bredForPortion, { where: { id: bredForId } });
+    return message.updatedRecord("BredFor", bredForId);
+  } catch (e) {
+    return message.updatedNoRecord("BredFor", bredForId);
+  }
 };
 
 const deleteOneBredFor = async (bredForId) => {
   try {
     await BredsFor.destroy(bredForId);
-    return {status: `BredFor ${bredForId} removed`}
-  } catch (e) {return {status: `BredFor ${bredForId} could not be removed`}}
+    return message.deletedRecord("BredFor", bredForId);
+  } catch (e) {
+    return message.deletedNoRecord("BredFor", bredForId);
+  }
 };
 
 module.exports = {
@@ -35,5 +53,5 @@ module.exports = {
   getOneBredFor,
   postOneBredFor,
   patchOneBredFor,
-  deleteOneBredFor
+  deleteOneBredFor,
 };
