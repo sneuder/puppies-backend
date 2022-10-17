b31f33663b7014863577e71e3fb0547f7bb0db9e
Esneider Ospina <sneuder8@gmail.com>
2022-10-17 16:51:38 -0500

    Add new message

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,42 +1,49 @@
 const error = {
-  status: "Something went wrong"
+  status: "Something went wrong",
 };
 
 const noRecords = (recordType) => {
   return {
-    status: `${recordType} no found`
-  }
+    status: `${recordType} no found`,
+  };
+};
+
+const noRecord = (recordType, idRecord) => {
+  return {
+    status: `${recordType} ${idRecord} not found`,
+  };
 };
 
 const updatedRecord = (recordType, idRecord) => {
   return {
-    status: `${recordType} ${idRecord} updated`
+    status: `${recordType} ${idRecord} updated`,
   };
 };
 
 const updatedNoRecord = (recordType, idRecord) => {
   return {
-    status: `${recordType} ${idRecord} with no updating`
+    status: `${recordType} ${idRecord} with no updating`,
   };
 };
 
 const deletedRecord = (recordType, idRecord) => {
   return {
-    status: `${recordType} ${idRecord} deleted`
+    status: `${recordType} ${idRecord} deleted`,
   };
 };
 
 const deletedNoRecord = (recordType, idRecord) => {
   return {
-    status: `${recordType} ${idRecord} not deleted`
+    status: `${recordType} ${idRecord} not deleted`,
   };
 };
 
 module.exports = {
   error,
   noRecords,
+  noRecord,
   updatedRecord,
   updatedNoRecord,
   deletedRecord,
-  deletedNoRecord
+  deletedNoRecord,
 };
