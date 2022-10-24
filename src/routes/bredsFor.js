f57ae5c84503dd1b0ac42a7d5b3a82b3af4ef74e
Esneider Ospina <sneuder8@gmail.com>
2022-10-24 09:04:51 -0500

    Add middlewares for bredsfor

--- src/routes/bredsFor.js
+++ src/routes/bredsFor.js
@@ -2,11 +2,36 @@ const { Router } = require("express");
 const router = Router();
 
 const bredsForControllers = require("../controllers/bredsForControllers");
+const middlewaresAuth = require("../middlewares/auth");
 
-router.get("/bredsfor/allBredsFor", bredsForControllers.getAllBredsFor);
-router.get("/bredsfor/:bredForId", bredsForControllers.getOneBredFor);
-router.post("/bredsfor/newBredFor", bredsForControllers.postOneBredFor);
-router.patch("/bredsfor/:bredForId", bredsForControllers.patchOneBredFor);
-router.delete("/bredsfor/:bredForId", bredsForControllers.deleteOneBredFor);
+router.get(
+  "/bredsfor/allBredsFor",
+  middlewaresAuth.user,
+  bredsForControllers.getAllBredsFor
+);
+
+router.get(
+  "/bredsfor/:bredForId",
+  middlewaresAuth.user,
+  bredsForControllers.getOneBredFor
+);
+
+router.post(
+  "/bredsfor/newBredFor",
+  middlewaresAuth.user,
+  bredsForControllers.postOneBredFor
+);
+
+router.patch(
+  "/bredsfor/:bredForId",
+  middlewaresAuth.user,
+  bredsForControllers.patchOneBredFor
+);
+
+router.delete(
+  "/bredsfor/:bredForId",
+  middlewaresAuth.user,
+  bredsForControllers.deleteOneBredFor
+);
 
 module.exports = router;
