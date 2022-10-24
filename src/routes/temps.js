7c16d514430ba4df1305c6e8a85d010a3dd924ee
Esneider Ospina <sneuder8@gmail.com>
2022-10-24 09:08:17 -0500

    Add middleware for temperaments

--- src/routes/temps.js
+++ src/routes/temps.js
@@ -2,11 +2,32 @@ const { Router } = require("express");
 const router = Router();
 
 const tempsControllers = require("../controllers/tempsControllers");
+const middlewaresAuth = require("../middlewares/auth");
 
-router.get("/temps/allTemps", tempsControllers.getAllTemps);
-router.get("/temps/:tempId", tempsControllers.getOneTemp);
-router.post("/temps/:tempId", tempsControllers.postOneTemp);
-router.patch("/temps/:tempId", tempsControllers.patchOneTemp);
-router.delete("/temps/:tempId", tempsControllers.deleteOneTemp);
+router.get(
+  "/temps/allTemps",
+  middlewaresAuth.user,
+  tempsControllers.getAllTemps
+);
+
+router.get("/temps/:tempId", middlewaresAuth.user, tempsControllers.getOneTemp);
+
+router.post(
+  "/temps/:tempId",
+  middlewaresAuth.user,
+  tempsControllers.postOneTemp
+);
+
+router.patch(
+  "/temps/:tempId",
+  middlewaresAuth.user,
+  tempsControllers.patchOneTemp
+);
+
+router.delete(
+  "/temps/:tempId",
+  middlewaresAuth.user,
+  tempsControllers.deleteOneTemp
+);
 
 module.exports = router;
