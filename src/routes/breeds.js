ff6696a6fd4ba6a7dff237684c4b805f69a3cc6d
Esneider Ospina <sneuder8@gmail.com>
2022-10-24 09:07:49 -0500

    Add middleware for breeds

--- src/routes/breeds.js
+++ src/routes/breeds.js
@@ -2,11 +2,36 @@ const { Router } = require("express");
 const router = Router();
 
 const breedsControllers = require("../controllers/breedsControllers");
+const middlewaresAuth = require("../middlewares/auth");
 
-router.get("/breeds/allBreeds", breedsControllers.getAllBreeds);
-router.get("/breeds/:breedId", breedsControllers.getOneBreed);
-router.post("/breeds/newBreed", breedsControllers.postOneBreed);
-router.patch("/breeds/:breedId", breedsControllers.patchOneBreed);
-router.delete("/breeds/:breedId", breedsControllers.deleteOneBreed);
+router.get(
+  "/breeds/allBreeds",
+  middlewaresAuth.user,
+  breedsControllers.getAllBreeds
+);
+
+router.get(
+  "/breeds/:breedId",
+  middlewaresAuth.user,
+  breedsControllers.getOneBreed
+);
+
+router.post(
+  "/breeds/newBreed",
+  middlewaresAuth.user,
+  breedsControllers.postOneBreed
+);
+
+router.patch(
+  "/breeds/:breedId",
+  middlewaresAuth.user,
+  breedsControllers.patchOneBreed
+);
+
+router.delete(
+  "/breeds/:breedId",
+  middlewaresAuth.user,
+  breedsControllers.deleteOneBreed
+);
 
 module.exports = router;
