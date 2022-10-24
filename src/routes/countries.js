c4bb3694fc1ec8cb0c012d4a4e975709bd038d39
Esneider Ospina <sneuder8@gmail.com>
2022-10-24 09:07:59 -0500

    Add middleware for countries

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -2,11 +2,36 @@ const { Router } = require("express");
 const router = Router();
 
 const countriesControllers = require("../controllers/countriesControllers");
+const middlewaresAuth = require("../middlewares/auth");
 
-router.get("/countries/allcountries", countriesControllers.getAllCountries);
-router.get("/countries/:countrydId", countriesControllers.getOneCountry);
-router.post("/countries/newCountry", countriesControllers.postOneCountry);
-router.patch("/countries/:countryId", countriesControllers.patchOneCountry);
-router.delete("/countries/:countryId", countriesControllers.deleteOneCountry);
+router.get(
+  "/countries/allcountries",
+  middlewaresAuth.user,
+  countriesControllers.getAllCountries
+);
+
+router.get(
+  "/countries/:countrydId",
+  middlewaresAuth.user,
+  countriesControllers.getOneCountry
+);
+
+router.post(
+  "/countries/newCountry",
+  middlewaresAuth.user,
+  countriesControllers.postOneCountry
+);
+
+router.patch(
+  "/countries/:countryId",
+  middlewaresAuth.user,
+  countriesControllers.patchOneCountry
+);
+
+router.delete(
+  "/countries/:countryId",
+  middlewaresAuth.user,
+  countriesControllers.deleteOneCountry
+);
 
 module.exports = router;
