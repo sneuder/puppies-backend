9ee4a1718d5e704c960b6309a896fc03e27f9374
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-08 17:23:49 -0500

    dogs from async to promises

--- src/services/dogsService.js
+++ src/services/dogsService.js
@@ -1,23 +1,23 @@
 const Database = require('../database/Dogs');
 
-const getAllDogs = async () => {
-  return await Database.getAllDogs();
+const getAllDogs = () => {
+  return Database.getAllDogs();
 };
 
-const getOneDog = async (dogId) => {
-  return await Database.getOneDog(dogId);
+const getOneDog = (dogId) => {
+  return Database.getOneDog(dogId);
 };
 
-const postOneDog = async (dog) => {
-  return await Database.postOneDog(dog);
+const postOneDog = (dog) => {
+  return Database.postOneDog(dog);
 };
 
-const patchOneDog = async (dogPortion, dogId) => {
-  return await Database.patchOneDog(dogPortion, dogId);
+const patchOneDog = (dogPortion, dogId) => {
+  return Database.patchOneDog(dogPortion, dogId);
 };
 
-const deleteOneDog = async (dogId) => {
-  return await Database.deleteOneDog(dogId);
+const deleteOneDog = (dogId) => {
+  return Database.deleteOneDog(dogId);
 };
 
 module.exports = {
